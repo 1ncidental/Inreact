a2b267a79d9f7f09007ec9c1079b195c22752afc
1ncidental <31199900+1ncidental@users.noreply.github.com>
2018-06-12 18:52:49 +1000

    Node Red API Call

--- Angular_Full_Version/js/controllers.js
+++ Angular_Full_Version/js/controllers.js
@@ -2129,7 +2129,7 @@ function codeEditorCtrl($scope) {
 
 function ngGridCtrl($scope, $http) {
     $scope.ngData = []
-$http.get('http://206.189.200.105:1337/recordeddata')
+$http.get('http://206.189.200.105:1880/api/recordeddata')
   .then(function(result) {
     $scope.ngData = result.data;
 });
